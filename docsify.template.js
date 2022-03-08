414815b2ff47147b27d2a22f87963ded0aea63a9
Jan Żaba <j.zaba@tim.pl>
2022-03-08 11:05:38 +0100

    Add dark-theme to docsify

--- docsify.template.js
+++ docsify.template.js
@@ -21,6 +21,7 @@ module.exports = options => {
       <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
       <link rel="stylesheet" href="${options.stylesheet}">
+      <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/docsify-darklight-theme@latest/dist/style.min.css" title="docsify-darklight-theme" type="text/css" />
     </head>
     
     <body>
@@ -31,6 +32,7 @@ module.exports = options => {
       <script src="//unpkg.com/docsify/lib/docsify.min.js"></script>
       <script src="//unpkg.com/docsify-plantuml/dist/docsify-plantuml.min.js"></script>
       <script src="//cdn.jsdelivr.net/npm/docsify/lib/plugins/zoom-image.min.js"></script>
+      <script src="//cdn.jsdelivr.net/npm/docsify-darklight-theme@latest/dist/index.min.js" type="text/javascript"></script>
     </body>
     
     </html>`;
